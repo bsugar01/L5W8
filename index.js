430ce45df760f187b544e060ae9e14bacca36cb0
bsugar01 <s2172274@students.ncl-coll.ac.uk>
2025-03-27 14:27:27 +0000

    asd

--- index.js
+++ index.js
@@ -5,9 +5,28 @@ const methodOverride = require("method-override");
 
 const app = express();
 
-mongoose.connect("mongodb://20.0.153.128:10999/SagarDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log("MongoDB Connected"))
-    .catch((err) => console.error("MongoDB Connection Error:", err));
+// mongoose.connect("mongodb://20.0.153.128:10999/fahimDB", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+// .then(() => console.log("MongoDB Connected"))
+// .catch((err) => console.error("MongoDB Connection Error:", err));
+
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(methodOverride("_method"));
+// app.set("view engine", "ejs");
+
+
+const studentSchema = new mongoose.Schema({ name: String, age: Number, course: String });
+const Student = mongoose.model("Student", studentSchema);
+
+
+app.get("/", (req, res) => {
+    // res.redirect("/students");
+    res.send("Hello from NCG");
+});
+
+
+app.listen(3000,()=>{console.log("Server is up on port 3000")})
